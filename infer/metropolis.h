fa73e69fc2ab07e055d6349bdf737d54bde2e5b1
Sergio Davis <sergdavis@gmail.com>
2016-11-17 11:12:53 -0300

    Metropolis has new callback ValidState; cleaning of unused code

--- infer/metropolis.h
+++ infer/metropolis.h
@@ -17,17 +17,6 @@
 #include "average.h"
 #include "function.h"
 
-inline double CheckDrift(double * w, int n)
-{
- int S = 0;
- for (int i=0;i<n-1;++i) 
- {
-  if (fabs(w[i+1]-w[i]) < 1.0E-6) continue;
-  S += ( (w[i+1]-w[i] < 0.0) ? -1 : 1 );
- }
- return double(S)/double(n-1);
-}
-
 inline double RejectionSigmoid(double R)
 {
  return (1.0+0.2*(1.0/(1.0+exp(7.0*(R-0.7)))-0.5));
@@ -46,11 +35,18 @@ template <class T> class Metropolis
     
     virtual bool OnProductionStep(T & x, long int step) { return true; }
 
+    virtual bool ValidState(const T & x) const { return true; }
+
     void SetDelta(double d) { delta = d; }
 
     bool Move(const RealFunction<T> & logprob, T & s, double & logprob_s)
     {
-     s.Mutate(delta);
+     while (1)
+     {
+      s.Mutate(delta);
+      if (ValidState(s)) break;
+      s.UndoMutation();
+     }
      double logpnew = logprob(s);
      double Q = logpnew-logprob_s;
      if (log(Random()) < Q) { logprob_s = logpnew; return true; }
@@ -78,35 +74,22 @@ template <class T> class Metropolis
      datasets.push_back(&data);
     }
 
+    void AdaptDelta()
+    {
+     delta *= RejectionSigmoid(RejectionRate()/100.0);
+    }
+
     void Simulate(const RealFunction<T> & logmodel, T & x, long int steps, long int burnin=0, long int adapt=0)
     {
      double logprobx = logmodel(x);
      Reset();
      long int n = 0;
-     //double * buffer = new double[NBUFFER];
-     //for (int i=0;i<NBUFFER;++i) buffer[i] = 0.0;
-     //int bc = 0;
-     //SimpleAverage xav(100);
      adapt_steps = adapt;
      while (1)
      {
       Sweep(logmodel, x, logprobx);
-      if ((adapt_steps > 0) && (n % adapt_steps == 0))
-      {
-       delta *= RejectionSigmoid(RejectionRate()/100.0);
-      }
-      if (!OnBurnInStep(x, n)) { return ; } //delete [] buffer; return;
-      //xav.Add(logprobx);
-      /*
-      if (xav.Full())
-      { 
-       if (bc == NBUFFER) bc = 0;
-       buffer[bc] = xav.Average();
-       bc++; 
-       double drift = CheckDrift(buffer, bc); 
-       if (((fabs(drift) > 0.0) && (fabs(drift) < fabs(buffer[bc-1])*CONVERGENCE_THRESHOLD))) break;
-      }
-      */
+      if ((adapt_steps > 0) && (n % adapt_steps == 0)) AdaptDelta();
+      if (!OnBurnInStep(x, n)) { return ; }
       n++;
       if (n >= burnin) break;
      }
@@ -133,9 +116,6 @@ template <class T> class Metropolis
    long int adapt_steps;
    std::vector< RealFunction<T> * > properties;
    std::vector< Block<double> * > datasets;
-
-//   const int NBUFFER = 10000;
-//   const double CONVERGENCE_THRESHOLD = 0.001;
 };
 
 #endif
