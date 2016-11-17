7b4a80bedd3f85aebd4a7f87d4b4b217179904e5
Sergio Davis <sergdavis@gmail.com>
2016-11-17 12:34:54 -0300

    Implemented MaxEntModel class

---
+++ infer/maxent.h
@@ -0,0 +1,53 @@
+/*
+ *
+ *
+ *
+ */
+
+#ifndef __MAXENT_H__
+#define __MAXENT_H__
+
+#include <list>
+#include "function.h"
+#include "state.h"
+
+template <class T> class FlatPrior: public RealFunction<T>
+{
+ public:
+   double operator()(const T & x) const override { return 0.0; }
+};
+
+template <class T> class MaxEntModel: public RealFunction<T>
+{
+ public:
+
+    MaxEntModel(int nparams)
+    {
+     f = new const RealFunction<T> *[nparams];
+     param_index = 0;
+    }
+
+    virtual ~MaxEntModel() { delete [] f; }
+
+    void SetPrior(const RealFunction<T> & lprior) { logprior = &lprior; }
+
+    void AddConstraint(const RealFunction<T> & R) { f[param_index++] = &R; }
+
+    void SetParams(const State & p) { params = p; }
+
+    double operator()(const T & x) const override
+    {
+     double logp = 0.0;
+     for (int i=0;i<params.Size();++i) logp -= params[i]*(*(f[i]))(x);
+     return logp+(*logprior)(x);
+    }
+
+ private:
+   const RealFunction<T> * logprior;
+   const RealFunction<T> ** f;
+   State params;
+   int param_index;
+};
+
+#endif
+
